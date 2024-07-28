64c17f8229f8cb2fb05b29cfa27254e54bdf9da9
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-07-28 21:41:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let productDiv =  document.querySelector(".product")
 let CategoryListDiv = document.querySelector(".CategoryList")
 
-let displayproduct= async ()=>{
-    productDiv.innerHTML = "";
-    CategoryListDiv.innerHTML=""
-    let allCat = [];
-    let product = await fetch("https://fakestoreapi.com/products");
-    let finalproduct = await product.json();
-    finalproduct.forEach(element => {
 
+let displayproduct= async (allCheckCat=[])=>{
+    productDiv.innerHTML = "";
+      
+    let allCat = [];        //an empty array
+    let product = await fetch("https://fakestoreapi.com/products");
+    let finalproduct = await product.json();        //20 Data elements
+    finalproduct.forEach(element => {
+        console.log(allCheckCat)
+        //Category Data
         if(!allCat.includes(element.category)){
-                 CategoryListDiv.innerHTML += `<label for="">
-                                            <input type="checkbox">${element.category}
-                                      </label>`
+                
+                 CategoryListDiv.innerHTML += `<label>
+                                            <input type="checkbox" onclick="categoryFilter()" value="${element.category}"> ${element.category}
+                                      </label>`;
                 allCat.push(element.category)
         }
 
-
+        if(allCheckCat.length==0){
+            allCheckCat=allCat;
+        }
+        if(allCheckCat.includes(element.category))
+        {
+        //Product Data
         productDiv.innerHTML += ` <div class="productItems">
                 <img src="${element.image}" alt="">
                 <h4>${element.category}</h4>
@@ -25,7 +33,23 @@ let displayproduct= async ()=>{
                     ${element.title}
                 </h3>
             </div>`
+        }
+        
     });
+    
 }
 
 displayproduct();
+
+let categoryFilter = ()=>{
+    let checkInput = document.querySelectorAll("input[type='checkbox']");
+    let checkdata = [];
+    checkInput.forEach((element)=>{
+        if(element.checked){
+            checkdata.push(element.value)
+        }
+    })
+    
+    displayproduct(checkdata)
+    
+}
