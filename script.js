f182ad68ceddd786469fb321ef25a36a8cf35862
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-07-28 15:43:28 +0500

    Some more updates in script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let productDiv =  document.querySelector(".product")
+let CategoryListDiv = document.querySelector(".CategoryList")
+
 let displayproduct= async ()=>{
     productDiv.innerHTML = "";
+    CategoryListDiv.innerHTML=""
+    let allCat = [];
     let product = await fetch("https://fakestoreapi.com/products");
     let finalproduct = await product.json();
     finalproduct.forEach(element => {
+
+        if(!allCat.includes(element.category)){
+                 CategoryListDiv.innerHTML += `<label for="">
+                                            <input type="checkbox">${element.category}
+                                      </label>`
+                allCat.push(element.category)
+        }
+
+
         productDiv.innerHTML += ` <div class="productItems">
                 <img src="${element.image}" alt="">
                 <h4>${element.category}</h4>
